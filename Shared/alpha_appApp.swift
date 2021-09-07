9561fd2e53c2530d987a5df410ee4802c5fb4696
Lincoln Anders <lincolnanders5@gmail.com>
2021-09-07 13:34:52 -0400

    Initial Commit

---
+++ Shared/alpha_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  alpha_appApp.swift
+//  Shared
+//
+//  Created by Lincoln Anders on 9/7/21.
+//
+
+import SwiftUI
+
+@main
+struct alpha_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
